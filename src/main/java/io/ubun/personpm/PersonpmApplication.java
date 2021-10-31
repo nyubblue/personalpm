ab2f63fdc70e99f6e16c2a6245067fa7d4423598
nyubblue <thanhbuynitu@gmail.com>
2021-10-31 22:15:27 +0700

    commit init

---
+++ src/main/java/io/ubun/personpm/PersonpmApplication.java
@@ -0,0 +1,13 @@
+package io.ubun.personpm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonpmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonpmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/ubun/personpm/PersonpmApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ubun.personpm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonpmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
